27e6c027f77c79c3886edd6511b2c5be902c43bf
aleinwu2018 <582946945@qq.com>
2019-11-28 20:13:27 +0800

    change readme

--- movie.py
+++ movie.py
@@ -13,15 +13,21 @@ if __name__ == '__main__':
     target = input("请输入要下载的视频地址:")
     command = "you-get "+target
     os.system(command)
-    filenames=os.listdir(os.getcwd())
-    for file in filenames:
-        print(file)
-        if os.path.splitext(file)[1] in [".mp4",".webm",".flv",]:
-            remember = file
-    print("视频下载完成!!!")
-    print("=======================================================")
-    print("开始转换....")
-    video = VideoFileClip(remember)
-    audio = video.audio
-    audio.write_audiofile(os.path.splitext(remember)[0]+'.mp3')
-    print("转换完毕！")
+    choice = input("是否转换成mp3格式(Y/N):")
+    if choice == 'Y':
+        filenames=os.listdir(os.getcwd())
+        for file in filenames:
+            print(file)
+            if os.path.splitext(file)[1] in [".mp4",".webm",".flv",]:
+                remember = file
+        print("视频下载完成!!!")
+        print("=======================================================")
+        print("开始转换....")
+        video = VideoFileClip(remember)
+        audio = video.audio
+        audio.write_audiofile(os.path.splitext(remember)[0]+'.mp3')
+        print("转换完毕！")
+    elif choice == 'N':
+        print("视频下载完成!!!")
+    else:
+        print('choice error!')
